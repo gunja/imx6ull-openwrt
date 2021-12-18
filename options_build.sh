035de7b1261cb75838b820666c1e09a5cc45b60f
Ivan F Gushchin @ DellCrow 18 <konara@ya.ru>
2021-12-19 03:39:19 +0400

    attempt to add debug uboot config

---
+++ options_build.sh
@@ -0,0 +1,4 @@
+export ARCH=arm
+export CROSS_COMPILE=arm-openwrt-linux-muslgnueabi-
+export STAGING_DIR=/home/gunja/Works/OpenWRT_porting/gunja_imx6ull-openwrt/staging_dir
+export PATH=$PATH:${STAGING_DIR}/toolchain-arm_cortex-a7+neon-vfpv4_gcc-7.5.0_musl_eabi/bin/
